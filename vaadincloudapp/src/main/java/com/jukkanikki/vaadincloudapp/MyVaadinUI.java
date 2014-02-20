86ef8d832e2d587ddca6fd203962d05888a52ecd
Jukka Nikki <nikki@exb.de>
2014-02-20 21:47:38 +0100

    Added seed of example code

--- vaadincloudapp/src/main/java/com/jukkanikki/vaadincloudapp/MyVaadinUI.java
+++ vaadincloudapp/src/main/java/com/jukkanikki/vaadincloudapp/MyVaadinUI.java
@@ -22,6 +22,14 @@ import com.vaadin.ui.Table.RowHeaderMode;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+/**
+ * 
+ * Example2:
+ * https://vaadin.com/book/vaadin7/-/page/components.table.html
+ * 
+ * @author nikkijuk
+ *
+ */
 @Theme("mytheme")
 @SuppressWarnings("serial")
 public class MyVaadinUI extends UI
@@ -126,6 +134,55 @@ public class MyVaadinUI extends UI
         
         layout.addComponent(sample);
         
+        Table table = new Table("Table with Cell Styles");
+        table.addStyleName("checkerboard");
+
+        // Add some columns in the table. In this example, the property
+        // IDs of the container are integers so we can determine the
+        // column number easily.
+        table.addContainerProperty("0", String.class, null, "", null, null);
+        for (int i=0; i<8; i++)
+            table.addContainerProperty(""+(i+1), String.class, null,
+                                 String.valueOf((char) (65+i)), null, null);
+
+        // Add some items in the table.
+        table.addItem(new Object[]{
+            "1", "R", "N", "B", "Q", "K", "B", "N", "R"}, new Integer(0));
+        table.addItem(new Object[]{
+            "2", "P", "P", "P", "P", "P", "P", "P", "P"}, new Integer(1));
+        for (int i=2; i<6; i++)
+            table.addItem(new Object[]{String.valueOf(i+1), 
+                         "", "", "", "", "", "", "", ""}, new Integer(i));
+        table.addItem(new Object[]{
+            "7", "P", "P", "P", "P", "P", "P", "P", "P"}, new Integer(6));
+        table.addItem(new Object[]{
+            "8", "R", "N", "B", "Q", "K", "B", "N", "R"}, new Integer(7));
+        table.setPageLength(8);
+
+        // Set cell style generator
+        table.setCellStyleGenerator(new Table.CellStyleGenerator() {
+            public String getStyle(Table source, Object itemId, Object propertyId) {
+                // Row style setting, not relevant in this example.
+                if (propertyId == null)
+                    return "green"; // Will not actually be visible
+
+                int row = ((Integer)itemId).intValue();
+                int col = Integer.parseInt((String)propertyId);
+                
+                // The first column.
+                if (col == 0)
+                    return "rowheader";
+                
+                // Other cells.
+                if ((row+col)%2 == 0)
+                    return "black";
+                else
+                    return "white";
+            }
+
+        });
+
+        layout.addComponent(table);
     }
 
 }
